ccdc44f247bcd9cc437244ea5e88e5005d329c32
Sandeep Verma <sandeep.verma@tredence.com>
2024-03-28 14:52:38 +0530

    code cleanup

--- src/components/common/ClearButton.tsx
+++
@@ -1,8 +0,0 @@
-import { Close } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-
-export default function ClearButton(props: any): JSX.Element {
-    return (
-        <IconButton {...props} size="small" sx={{ mr: 2 }}><Close /></IconButton>
-    );
-}
